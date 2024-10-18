67e29c10571243c1c7582b58e7ae38cce433dd54
Jay <77417138+tobinja1@users.noreply.github.com>
2024-10-17 22:20:47 -0400

    Get audio input registered

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,51 +1,53 @@
-var recordCheckbox = document.getElementById("record-checkbox");
-var recordValue = 0;
+// Get createDevice from the rnbo.js library
+const { createDevice } = RNBO;
 
-function record(){
-    if(recordCheckbox.checked == true) {
-        console.log("checked");
-        recordValue = 1;
+// Create AudioContext
+let WAContext = window.AudioContext || window.webkitAudioContext;
+let context = new WAContext();
+
+const setup = async () => {
+    let rawPatcher = await fetch("assets/rnbo/recorder.json");
+    let patcher = await rawPatcher.json();
+
+    var device = await createDevice({ context, patcher });
+    
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    // Create gain node and connect it to audio output
+    const source = context.createMediaStreamSource(stream);
+
+    var recordCheckbox = document.getElementById("record-checkbox");
+    var recordValue = 0;
+
+    function record() {
+        if(recordCheckbox.checked == true) {
+            console.log("checked");
+            recordValue = 1;
+        }
+        else {
+            console.log("unchecked");
+            recordValue = 0;
+
+        }
     }
-    else {
-        console.log("unchecked");
-        recordValue = 0;
-    }
+  
+  
+    recordToggle = device.parametersById.get("recordToggle");
+    recordToggle.value = recordValue;
+    // This connects the device to audio output, but you may still need to call context.resume()
+    // from a user-initiated function.
+    source.connect(device.node);
+    
+    device.node.connect(context.destination);
+    context.suspend();
+};
+
+// We can't await an asynchronous function at the top level, so we create an asynchronous
+// function setup, and then call it without waiting for the result.
+setup();
+
+
+function startAudio() {
+  context.resume();
 }
 
-(async () => {
-  
-    const ctx = new (window.AudioContext || window.webkitAudioContext)();
-    const response = await fetch("/assets/rnbo/recorder.json");
-    const patcher = await response.json();
-    let setup = false;
-    
-    const device = await RNBO.createDevice({
-      context: ctx,
-      patcher
-    });
-    
-    const run = async () => {
-      if (setup) return;
-      setup = true;
-      ctx.resume();
-  
-      try {
-        // Access user's microphone
-        const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
-        
-        // Create Media Stream source
-        const source = ctx.createMediaStreamSource(stream);
-        
-        // Connect source to RNBO Device
-        source.connect(device.node);
-        
-        // Connect RNBO Device to destination output
-        device.node.connect(ctx.destination);
-        
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    
-
-  })();
+startAudio();
