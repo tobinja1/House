595414ffef317ce235c9112ee4c80eb69c394570
Jay <77417138+tobinja1@users.noreply.github.com>
2024-10-17 22:40:25 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,18 @@ const { createDevice } = RNBO;
 let WAContext = window.AudioContext || window.webkitAudioContext;
 let context = new WAContext();
 
+var recordCheckbox = document.getElementById("record-checkbox");
+var recordValue = 0;
+var recordToggle;
+
+recordCheckbox.addEventListener('change', function() {
+    if (this.checked) {
+        recordValue = 1;
+    } else {
+      recordValue = 0;
+    }
+  });
+
 const setup = async () => {
     let rawPatcher = await fetch("assets/rnbo/recorder.json");
     let patcher = await rawPatcher.json();
@@ -14,25 +26,9 @@ const setup = async () => {
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
     // Create gain node and connect it to audio output
     const source = context.createMediaStreamSource(stream);
-
-    var recordCheckbox = document.getElementById("record-checkbox");
-    var recordValue = 0;
-
-    function record() {
-        if(recordCheckbox.checked == true) {
-            console.log("checked");
-            recordValue = 1;
-        }
-        else {
-            console.log("unchecked");
-            recordValue = 0;
-
-        }
-    }
-  
   
     recordToggle = device.parametersById.get("recordToggle");
-    recordToggle.value = recordValue;
+    // recordToggle.value = recordValue;
     // This connects the device to audio output, but you may still need to call context.resume()
     // from a user-initiated function.
     source.connect(device.node);
@@ -43,11 +39,19 @@ const setup = async () => {
 
 // We can't await an asynchronous function at the top level, so we create an asynchronous
 // function setup, and then call it without waiting for the result.
-setup();
+setup().then(function(){
+    recordCheckbox.addEventListener('change', function() {
+        if (this.checked) {
+            recordToggle.value = 1;
+            console.log(recordToggle.value)
+        } else {
+            recordToggle.value = 0;
+            console.log(recordToggle.value)
+        }
+      });
+});
 
 
 function startAudio() {
   context.resume();
 }
-
-startAudio();
